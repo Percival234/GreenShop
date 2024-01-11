fea90c81cc0d942d16068149d127a4f4a2ed3124
Percival234 <abaduyaprise@gmail.com>
2024-01-11 15:55:38 +0200

    text-center class adding

--- src/Components/UI/Titles/SubTitle/SubTitle.jsx
+++ src/Components/UI/Titles/SubTitle/SubTitle.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './SubTitle.scss';
 
 function SubTitle({ children, center }) {
-  return <h3 className={`${center ? 'text-center' : ''} sub-title`}>{children}</h3>;
+  return <h3 className={`${center ? 'centered' : ''} sub-title`}>{children}</h3>;
 }
 
 SubTitle.propTypes = {
