4d3b7edb0e9a04f7badf439890afbb97040b204d
Percival234 <abaduyaprise@gmail.com>
2024-01-12 14:10:42 +0200

    last last commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+
 import './Styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
-);
+root.render(<App />);
